985a59ca495d86e55ec52d893055299e975877b9
eki89 <fabian@oveleon.de>
2020-02-21 13:33:29 +0100

    Bugfix: Insert tags method renamed.

--- src/EventListener/InsertTagsListener.php
+++ src/EventListener/InsertTagsListener.php
@@ -40,7 +40,7 @@ class InsertTagsListener
     /**
      * @return string|false
      */
-    public function __invoke(string $tag, bool $useCache, $cacheValue, array $flags)
+    public function onReplaceInsertTags(string $tag, bool $useCache, $cacheValue, array $flags)
     {
         $elements = explode('::', $tag);
         $key = strtolower($elements[0]);
