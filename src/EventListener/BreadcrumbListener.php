264b886710786294194787db426e3d9b4c6ef524
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2023-03-13 10:06:07 +0100

    Add the support for breadcrumb.

---
+++ src/EventListener/BreadcrumbListener.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Oveleon Contao Glossary Bundle.
+ *
+ * @package     contao-glossary-bundle
+ * @license     AGPL-3.0
+ * @author      Fabian Ekert        <https://github.com/eki89>
+ * @author      Sebastian Zoglowek  <https://github.com/zoglo>
+ * @copyright   Oveleon             <https://www.oveleon.de/>
+ */
+
+namespace Oveleon\ContaoGlossaryBundle\EventListener;
+
+use Contao\Config;
+use Contao\Input;
+use Contao\StringUtil;
+use Oveleon\ContaoGlossaryBundle\Glossary;
+use Oveleon\ContaoGlossaryBundle\GlossaryItemModel;
+
+/**
+ * @internal
+ */
+class BreadcrumbListener
+{
+    public function __invoke(array $items): array
+    {
+        // Set the item from the auto_item parameter
+        if (!isset($_GET['items']) && isset($_GET['auto_item']) && Config::get('useAutoItem')) {
+            Input::setGet('items', Input::get('auto_item'));
+        }
+
+        if (($alias = Input::get('items')) && ($glossaryItem = GlossaryItemModel::findPublishedByIdOrAlias($alias)) !== null) {
+            // Mark the last item as inactive
+            $items[\count($items) - 1]['href'] = $GLOBALS['objPage']->getFrontendUrl();
+            $items[\count($items) - 1]['isActive'] = false;
+
+            // Add the new item
+            $items[] = [
+                'isRoot' => false,
+                'isActive' => true,
+                'href' => Glossary::generateUrl($glossaryItem),
+                'title' => StringUtil::specialchars($glossaryItem->keyword),
+                'link' => $glossaryItem->keyword,
+                'data' => $glossaryItem->row(),
+                'class' => '',
+            ];
+        }
+
+        return $items;
+    }
+}
