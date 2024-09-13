0b9f7c6d684321e8f173a08367f668868fc522f7
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-09-13 15:56:21 +0200

    Add `AliasException` instead of base class

---
+++ src/Utils/AliasException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Oveleon Contao Glossary Bundle.
+ *
+ * @package     contao-glossary-bundle
+ * @license     AGPL-3.0
+ * @author      Sebastian Zoglowek    <https://github.com/zoglo>
+ * @author      Fabian Ekert          <https://github.com/eki89>
+ * @author      Daniele Sciannimanica <https://github.com/doishub>
+ * @copyright   Oveleon               <https://www.oveleon.de/>
+ */
+
+namespace Oveleon\ContaoGlossaryBundle\Utils;
+
+class AliasException extends \Exception
+{
+}
